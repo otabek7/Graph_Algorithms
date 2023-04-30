82ab96860e0f3deedcf26652305a344e7ba0c523
mavlonovo <mavlonovo@tamu.edu>
2023-04-29 22:17:05 -0500

    final_commit

--- assignment-graph-algorithms/src/graph-algorithms.h
+++ assignment-graph-algorithms/src/graph-algorithms.h
@@ -31,7 +31,7 @@
  * @return std::list<value_type<T>> list of nodes along shortest path including initial_node and destination_node, empty if no path exists
  */
 template <typename T>
-std::list<value_type<T>> dijkstrasAlgorithm(const WeightedGraph<T>& graph, vertex_type<T> initial_node, vertex_type<T> destination_node)
+std::list<value_type<T>> dijkstrasAlgorithm(const WeightedGraph<T> &graph, vertex_type<T> initial_node, vertex_type<T> destination_node)
 {
     std::unordered_map<value_type<T>, weight_type<T>> distances;
     std::unordered_map<value_type<T>, std::optional<value_type<T>>> predecessors;
@@ -41,8 +41,52 @@ std::list<value_type<T>> dijkstrasAlgorithm(const WeightedGraph<T>& graph, verte
     // TODO implement Dijkstra's Algorithm
 
     // TODO create list by walking backwards through predecessors from the end
+    // s = [] # Set
+    //  q = [] # Priority Queue
+    //  q.extend([v for v in graph])
+    initializeSingleSource(graph, initial_node, distances, predecessors);
 
-    return std::list<value_type<T>>();
+    //extend function implement 
+    for (auto &v : graph)
+    {
+        q.push(v.first);
+    }
+    while (q.size() > 0)
+    {
+        //set u to be min
+        value_type<T> u = q.top();
+        if (distances[u] == infinity<T>())
+            break;
+        q.pop();//no such thing as remove
+
+        if (s.find(u) == s.end())//check if it is in, if so insert
+            s.insert(u);
+        for (auto &pair : graph.at(u))
+        {
+            auto v = pair.first;
+            if (s.find(v) != s.end())
+                continue;
+            auto w = pair.second;
+            bool r = relax<T>(u, v, w, distances, predecessors);
+            if (r)
+                updateHeap<T>(q, distances);
+        }
+    }
+
+    std::list<value_type<T>> l;//initialize a list
+
+    auto node = destination_node;
+    while (predecessors[node].has_value())
+    {
+        l.push_front(node);
+        node = predecessors[node].value();
+    }
+    if (l.size() > 0 || initial_node == destination_node)
+        l.push_front(initial_node);
+
+    // TODO create list by walking backwards through predecessors from the end
+
+    return l;
 }
 
 #include "top-sort-helpers.h"
@@ -55,14 +99,42 @@ std::list<value_type<T>> dijkstrasAlgorithm(const WeightedGraph<T>& graph, verte
  * @return std::list<value_type<T>> list of nodes in a topological order, or an empty list if no such ordering exists
  */
 template <typename T>
-std::list<value_type<T>> topologicalSort(const WeightedGraph<T>& graph)
+std::list<value_type<T>> topologicalSort(const WeightedGraph<T> &graph)
 {
     std::unordered_map<value_type<T>, int> indegrees;
     std::unordered_map<value_type<T>, int> topological_numbers;
 
     // TODO
+    std::queue<value_type<T>> q;
+    std::list<value_type<T>> l;
+    computeIndegrees(graph, indegrees);
 
-    return std::list<value_type<T>>();
+    for (auto &v : graph)
+    {
+        if (indegrees[v.first] == 0)
+        {
+            q.push(v.first);
+        }
+    }
+    while ((q).size() > 0)
+    {
+        value_type<T> v = q.front();
+        q.pop();
+        l.push_back(v);
+
+        for (auto adj_v : graph.at(v))
+        {
+            indegrees[adj_v.first] -= 1;
+            if (indegrees[adj_v.first] == 0)
+                q.push(adj_v.first);
+        }
+    }
+    if ((l).size() != (graph).size())
+        l.clear();
+
+    return l;
+
+    // return std::list<value_type<T>>();
 }
 
 template <typename T>
@@ -138,8 +210,8 @@ std::istream &operator>>(std::istream &i, WeightedGraph<T> &graph)
         }
     }
 
-    if(i.eof() and i.fail())
+    if (i.eof() and i.fail())
         i.clear(std::ios::eofbit);
-    
+
     return i;
 }
--- assignment-graph-algorithms/src/top-sort-helpers.h
+++ assignment-graph-algorithms/src/top-sort-helpers.h
@@ -6,6 +6,15 @@
 #include "graph-types.h"
 
 template <typename T>
-void computeIndegrees(const WeightedGraph<T>& graph, std::unordered_map<value_type<T>, int>& indegrees) {
+void computeIndegrees(const WeightedGraph<T> &graph, std::unordered_map<value_type<T>, int> &indegrees)
+{
     // TODO store the indegree for each vertex in the graph in the indegrees map
+    for (auto &vertex : graph)
+        indegrees[vertex.first] = 0;
+
+    for (auto &adj_list : graph)
+    {
+        for (auto &vertex : adj_list.second)
+            indegrees[vertex.first] += 1;
+    }
 }
